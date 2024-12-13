080e19a9c86d4d228395f543b567ad0d473419fd
aabdi <arsanbosss@gmail.com>
2024-12-13 17:09:50 +0100

    push

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { ChartComponent } from './chart/chart.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Page d'accueil
+  { path: 'list', component: ListComponent }, // Page de liste paginée
+  { path: 'chart', component: ChartComponent }, // Page graphique
+];
